4bad7d2399aa94c63fc049651bba077e883981cb
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2018-01-29 09:45:44 +0800

    refine: add method requirement TODO prevent Replay Attrack

--- index.js
+++ index.js
@@ -11,27 +11,33 @@ const app = new Koa();
 app.use(bodyParser());
 
 async function githubEvt(ctx, next){
+    // TODO 防止重放攻击
+    console.log(ctx.body)
     if (ctx.url === "/git") {
-        let requestHash;
-        let gitSignature;
-        if (GITLAB_TOKEN) {
-            requestHash = GITLAB_TOKEN;
-            gitSignature = ctx.header['X-Gitlab-Token'];
-        }
-        if (GITHUB_TOKEN) {
-            requestHash = `sha1=${crypto.createHmac('sha1', GITHUB_TOKEN).update(JSON.stringify(ctx.request.body)).digest("hex")}`;
-            gitSignature = ctx.header['X-Hub-Signature'];
-        }
-        // console.log(requestHash);
-        if (requestHash === gitSignature) {
-            ctx.body = 'Restarting node server!';
-            let cmd = execFile("sh", ["start.sh"], (err, stdout, stderr) => {
-                if (err){
-                    ctx.status = 503;
-                    return ctx.body = `Restarting node server Failed! Error${err}`
-                }
-            });
-            return;
+        if (ctx.request.body || ctx.request.method === "POST") {
+            let requestHash;
+            let gitSignature;
+            if (GITLAB_TOKEN) {
+                requestHash = GITLAB_TOKEN;
+                gitSignature = ctx.header['X-Gitlab-Token'];
+            }
+            if (GITHUB_TOKEN) {
+                requestHash = `sha1=${crypto.createHmac('sha1', GITHUB_TOKEN).update(JSON.stringify(ctx.request.body)).digest("hex")}`;
+                gitSignature = ctx.header['X-Hub-Signature'];
+            }
+            // console.log(requestHash);
+            if (requestHash === gitSignature) {
+                ctx.body = 'Restarting node server!';
+                let cmd = execFile("sh", ["start.sh"], (err, stdout, stderr) => {
+                    if (err){
+                        ctx.status = 503;
+                        return ctx.body = `Restarting node server Failed! Error${err}`
+                    }
+                });
+                return;
+            }
+        } else {
+            return ctx.status = 404;
         }
     }
     await next();
